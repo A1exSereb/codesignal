50e0f2b2239c0899593904499a9ba237e2987674
Alex <dtmakus@gmail.com>
2022-06-14 16:12:31 +0600

    Why are we still here? Just to suffer? Every night, I can feel my leg... And my arm... even my fingers...

---
+++ differentSymbolsNaive.js
@@ -0,0 +1,10 @@
+//can be optimized 
+const solution = (s) => s.split('').filter((el,i,arr) => i === arr.lastIndexOf(el)).length
+//we can just remove split and get count of uniq chars with loop and lastIndex
+const solution2 = (s) => {
+    let counter = 0;
+    for(let i = 0, len = s.length; i < len;i++){
+        if(i === s.lastIndexOf(s[i])) counter++
+    }
+    return counter;
+}
